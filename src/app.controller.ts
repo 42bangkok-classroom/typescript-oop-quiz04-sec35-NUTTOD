ca0e0f6b77d622bbc4dde6d85c94ff53047503a1
NUTTOD <nuttod88@gmail.com>
2026-03-23 10:54:41 +0700

    add ,

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { UserService } from './app.service';
+import { AppService, UserService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly userService: UserService
+    private readonly userService: UserService,
   ) {}
 
   @Get()
